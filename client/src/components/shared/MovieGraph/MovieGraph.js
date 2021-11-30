e599829475db3fc0715f3d9edc5915a886beee1b
tonyv <tonyvilla77@gmail.com>
2021-11-30 01:26:44 -0800

    added graph legend

--- client/src/components/shared/MovieGraph/MovieGraph.js
+++ client/src/components/shared/MovieGraph/MovieGraph.js
@@ -157,7 +157,30 @@ function MovieGraph({ title }) {
 
   return (
     <div className="movie-graph">
-      <Radar {...commonProperties} />
+      <Radar
+        {...commonProperties}
+        legends={[
+          {
+            anchor: 'top-left',
+            direction: 'column',
+            translateX: -50,
+            translateY: -40,
+            itemWidth: 80,
+            itemHeight: 20,
+            itemTextColor: '#999',
+            symbolSize: 12,
+            symbolShape: 'circle',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemTextColor: '#000',
+                },
+              },
+            ],
+          },
+        ]}
+      />
     </div>
   );
 }
