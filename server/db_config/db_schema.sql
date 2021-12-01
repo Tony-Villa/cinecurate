9d0e0ee3f59aeef1d81f2c3635e9f72f6e5b21cc
tonyv <tonyvilla77@gmail.com>
2021-11-30 16:41:22 -0800

    changed schema

--- server/db_config/db_schema.sql
+++ server/db_config/db_schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(1024) NOT NULL,
-    first_name VARCHAR(50) NOT NULL
+    first_name VARCHAR(50) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
