dcb9cc7dae79b98dfb77e2cd7ae5a0460d4035d8
tonyv <tonyvilla77@gmail.com>
2021-12-02 10:48:28 -0800

    changed review query

--- server/controllers/reviewControllers.js
+++ server/controllers/reviewControllers.js
@@ -18,7 +18,7 @@ const showReviews = async (req, res, next) => {
 const showUserReviews = async (req, res, next) => {
   try {
     const userReviews = await pool.query(
-      'SELECT reviews.id, first_name, username, movie_title, review_type, rating, review, reviews.created_at FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE user_id = $1 ORDER BY reviews.created_at DESC',
+      'SELECT reviews.id, first_name, username, movie_title, movie_id, review_type, rating, review, reviews.created_at FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE user_id = $1 ORDER BY reviews.created_at DESC',
       [req.params.user_id]
     );
 
